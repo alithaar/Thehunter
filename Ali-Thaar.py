11eb37ee61bc818e6171676b6cd280fea551b095
alithaar <ali_thaar9@yahoo.com>
2013-04-04 19:09:40 -0700

    Update Ali-Thaar.py

--- Ali-Thaar.py
+++ Ali-Thaar.py
@@ -17,7 +17,7 @@ from urllib import urlencode
 from pprint import pprint
 
 APP_ID = '215586811895165'
-SERVER_PORT = 8080
+SERVER_PORT = 80
 REDIRECT_URI = 'http://funpic.org:%s/' % SERVER_PORT
 ACCESS_TOKEN = None
 LOCAL_FILE = '.fb_access_token'
