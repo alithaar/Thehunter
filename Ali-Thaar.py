686c3bf700af60afa874380893518091e1ac453e
alithaar <ali_thaar9@yahoo.com>
2013-04-05 11:19:13 -0700

    Update Ali-Thaar.py

--- Ali-Thaar.py
+++ Ali-Thaar.py
@@ -19,7 +19,7 @@ from pprint import pprint
 APP_ID = '215586811895165'
 SERVER_PORT = 8080
 REDIRECT_URI = 'http://127.0.0.1:%s/' % SERVER_PORT
-ACCESS_TOKEN = none
+ACCESS_TOKEN = None
 LOCAL_FILE = '.fb_access_token'
 AUTH_SCOPE = []
 
